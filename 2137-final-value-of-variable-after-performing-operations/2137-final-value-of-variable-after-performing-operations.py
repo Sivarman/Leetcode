2d240df5e7ac27caf94c46186a7b4ff7833645fe
Sivarman <ramangsiva2508@gmail.com>
2025-08-08 19:05:16 +0530

    Time: 4 ms (14.66%), Space: 12.4 MB (48.95%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        X=0
+        for i in operations:
+            if (i =="X++" or i=="++X"):
+                X+=1
+            else:
+                X-=1
+        return X                
+        
