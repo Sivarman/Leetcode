263a9c63e4dce102d05d51b9921a1b7830a9a4ac
ramansiva <ramangsiva2508@gmail.com>
2025-12-12 23:15:11 +0530

    Time: 0 ms (100%), Space: 42.9 MB (35.69%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        int x1 = points[0][0], x2 = points[1][0], x3 = points[2][0];
+        int y1 = points[0][1], y2 = points[1][1], y3 = points[2][1];
+
+        double ans = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0;
+
+        if (ans == 0) {
+            return false;
+        }
+        return true;
+    }
+}
